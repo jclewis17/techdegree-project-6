095ae1c9d58ffc6c47c7424212e3535b7ba928c5
Jeremiah Lewis <jclewis1914@gmail.com>
2025-03-02 12:03:58 -0700

    Phrases Array
    Created an array of 5 phrases.

--- app.js
+++ app.js
@@ -2,4 +2,15 @@
 qwerty = document.querySelector('#qwerty');
 phrase = document.querySelector('#phrase');
 mainButton = document.querySelector('.btn__reset');
-wrongGuess = 0;
+wrongGuessCounter = 0;
+
+//PHRASES ARRAY
+const phrases = [
+    "BREAK A LEG",
+    "PIECE OF CAKE",
+    "EASY COME EASY GO",
+    "THE EARLY BIRD GETS THE WORM",
+    "PRACTICE MAKES PERFECT"
+];
+
+console.log(phrases);
